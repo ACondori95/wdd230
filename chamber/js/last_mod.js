3e9eb288d63def8c9a64aa9cf8d123fa87e8fbed
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-03-04 00:33:24 -0300

    some changes

--- chamber/js/last_mod.js
+++
@@ -1,2 +0,0 @@
-var lastMod = document.lastModified;
-document.querySelector('#last-update').textContent = "Last Updated: " + lastMod;
--- chamber/js/windchill.js
+++
@@ -1,5 +0,0 @@
-let temp = document.getElementById('temp').textContent;
-let speed = document.getElementById('wind-speed').textContent;
-let windCalc = 35.74 + (0.6215 * temp) - 35.74 * Math.pow(speed, 0.16) + 0.4275 * (temp * Math.pow(speed, 0.16));
-
-document.getElementById('wind-chill').textContent = windCalc.toFixed(0);
