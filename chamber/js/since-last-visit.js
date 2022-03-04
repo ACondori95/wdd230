aebdb7bc4f1d1b8d2a49d2ba9007a3afb08e0e86
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-03-04 12:56:28 -0300

    since last visit js

---
+++ chamber/js/since-last-visit.js
@@ -0,0 +1,20 @@
+let lastvisit = Number(window.localStorage.getItem('last-visit-date'));
+
+if (lastvisit != 0) {
+  let thisvisit = Date.now();
+  let FACTOR = 86400000;
+  let daysBetween = (thisvisit - lastvisit) / FACTOR;
+
+  if (daysBetween.toFixed(0) != 1) {
+    document.querySelector('#lastvisit').textContent = 'It has been ' + daysBetween.toFixed(0) + ' days since your last visit.';
+    window.localStorage.setItem('last-visit-date', thisvisit);
+    thisvisit = NULL;
+  } else {
+    document.querySelector('#lastvisit').textContent = 'It has been ' + daysBetween.toFixed(0) + ' day sinceyour last visit.';
+    window.localStorage.setItem('last-visit-date', thisvisit);
+    thisvisit = NULL;
+  }
+} else {
+  window.localStorage.setItem('last-visit-date', Date.now());
+  document.querySelector('#lastvisit').textContent = 'This is your first visit! Welcome!';
+}
