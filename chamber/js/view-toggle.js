cf16ba5d3b4aadeadf4e2c524bcda6081ed784ed
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-03-17 10:01:50 -0300

    directory base

--- chamber/js/view-toggle.js
+++ chamber/js/view-toggle.js
@@ -1,34 +1,13 @@
-const viewtype = 'O';
 const ww = window.innerWidth;
 
 if (ww >= 592 && ww <= 991) {
   toggleView();
-  viewtype = 'M';
 }
 
 function toggleView() {
   document.getElementById('cardviewBtn').classList.toggle('open');
-  document.getElementById('listviewBtn').classList.toggle('open');
   document.getElementById('bizdir').classList.toggle('open');
 }
 
-const lv = document.getElementById('listviewBtn');
-lv.onclick = toggleView;
-
 const c = document.getElementById('cardviewBtn');
 c.onclick = toggleView;
-
-function checkWidth() {
-  ww = window.innerWidth;
-  if (ww >= 592 && ww <= 991) {
-    if (viewtype != 'M') {
-      toggleView();
-      viewtype = 'M';
-    }
-  } else {
-    if (viewtype == 'M') {
-      toggleView();
-      viewtype = 'O';
-    }
-  }
-}
